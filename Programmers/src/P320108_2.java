e226fd98cbe86d3561ae0bc1e7091267192dc086
chanhl22 <chanhlee@student.42seoul.kr>
2021-10-07 14:53:35 +0900

    요일 구하기 알고리즘 (윤년 추가)

--- Programmers/src/P320108_2.java
+++ Programmers/src/P320108_2.java
@@ -28,3 +28,33 @@ public class P320108_2 {
         System.out.println(ans);
     }
 }
+
+//윤년 추가
+//윤년 여부
+//      void IsLeapYear(int year)
+//      {
+//          if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//              month_day[1] = 29;
+//      }
+//
+//    // 날짜이름
+//    string getDayName(int year, int month, int day)
+//    {
+//        int total_day = 0;
+//
+//        // 년
+//        IsLeapYear(year);
+//
+//        // 달
+//        for(int i=1; i<month; i++)
+//        {
+//            total_day += month_day[i-1];
+//        }
+//        // 일
+//        total_day += day-1;
+//
+//        // 2016.1.1 = 금
+//        int answer_day = (5 + total_day) % 7;
+//
+//        return str_day[answer_day];
+//    }
