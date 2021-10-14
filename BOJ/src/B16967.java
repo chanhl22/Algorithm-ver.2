29dd4ca18dbd3fa63de8030feb00e5b9330e7954
chanhl22 <chanhlee@student.42seoul.kr>
2021-10-14 09:00:44 +0900

    16967

--- BOJ/src/B16967.java
+++ BOJ/src/B16967.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
-public class B16967{
+public class B16967 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         int w = sc.nextInt();
         int x = sc.nextInt();
         int y = sc.nextInt();
-        int[][] a = new int[h+x][w+y];
-        for (int i=0; i<h+x; i++) {
-            for (int j=0; j<w+y; j++) {
+        int[][] a = new int[h + x][w + y];
+        for (int i = 0; i < h + x; i++) {
+            for (int j = 0; j < w + y; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
-        for (int i=0; i<h; i++) {
-            for (int j=0; j<w; j++) {
-                a[i+x][j+y] -= a[i][j];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                a[i + x][j + y] -= a[i][j];
             }
         }
-        for (int i=0; i<h; i++) {
-            for (int j=0; j<w; j++) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
@@ -27,3 +27,38 @@ public class B16967{
     }
 }
 
+//import java.util.Scanner;
+//
+//public class B16967 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        int x = sc.nextInt();
+//        int y = sc.nextInt();
+//        int[][] a = new int[n][m];
+//        int[][] b = new int[n + x][m + y];
+//        for (int i = 0; i < n + x; i++) {
+//            for (int j = 0; j < m + y; j++) {
+//                b[i][j] = sc.nextInt();
+//            }
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (i < x && j < m) {
+//                    a[i][j] = b[i][j];
+//                } else if (i < n && j < y) {
+//                    a[i][j] = b[i][j];
+//                } else if (i >= x && i < n && j >= y && j < m) {
+//                    a[i][j] = b[i][j] - a[i - x][j - y];
+//                }
+//            }
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                System.out.print(a[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
