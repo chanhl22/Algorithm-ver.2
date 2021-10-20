d56ee8e8c5aab3e2a236ad56120dcc59e61bb833
chanhl22 <chanhlee@student.42seoul.kr>
2021-10-20 22:32:21 +0900

    3678 need to refactoring

--- BOJ/src/B3678.java
+++ BOJ/src/B3678.java
@@ -5,22 +5,40 @@ public class B3678 {
         Scanner sc = new Scanner(System.in);
         int[] dx = {-1, -1, 0, 1, 1, 0};
         int[] dy = {1, 0, -1, -1, 0, 1};
-        int[] r = {1, 2, 3, 4, 5};
-        int[][] a = new int[51][51];
-        int[][] b = new int[51][51];
-        int x = 25;
-        int y = 25;
+        int[] r = {0, 1, 0, 0, 0, 0}; //사용한 숫자
+        int[][] a = new int[501][501]; //501로 한 이유는 딱히 없음, 201로 하면 범위를 넘어가길래..
+        int[][] b = new int[501][501];
+        int x = 250;
+        int y = 250;
         a[x][y] = 1;
         b[x][y] = 1;
-        b[x][y + 1] = 2;
-        b[x - 1][y + 1] = 3;
-        b[x - 1][y] = 4;
-        b[x][y - 1] = 5;
         int cur = 2;
-        for (int level = 1; level <= 4; level++) {
+        for (int level = 1; level <= 100; level++) { //이거도 대충 100보단 작겠지 싶어서..
             x += dx[5];
             y += dy[5];
             a[x][y] = cur++;
+            boolean[] check1 = new boolean[6];
+            for (int j = 0; j < 6; j++) {
+                int nx = x + dx[j];
+                int ny = y + dy[j];
+                for (int l = 1; l <= 5; l++) {
+                    if (b[nx][ny] == l) {
+                        check1[l] = true;
+                    }
+                }
+            }
+            int min1 = -1;
+            int temp1 = 0;
+            for (int j = 1; j < 6; j++) {
+                if (min1 == -1 || min1 > r[j]) {
+                    if (check1[j] == false) {
+                        min1 = r[j];
+                        temp1 = j;
+                    }
+                }
+            }
+            b[x][y] = temp1;
+            r[temp1]++;
             for (int k = 0; k < 6; k++) {
                 int loop = level;
                 if (k == 0) loop -= 1;
@@ -28,38 +46,36 @@ public class B3678 {
                     x += dx[k];
                     y += dy[k];
                     a[x][y] = cur++;
-                    if (cur > 6) {
-                        boolean[] check = new boolean[6];
-                        for (int j = 0; j < 6; j++) {
-                            int nx = x + dx[j];
-                            int ny = y + dy[j];
-                            for (int l = 1; l <= 5; l++) {
-                                if (a[nx][ny] == l) {
-                                    check[l] = true;
-                                }
-                            }
-                        }
-                        for (int j = 1; j < 6; j++) {
-                            if (check[j] == false) {
-                                b[x][y] = j;
-                                break;
+                    boolean[] check2 = new boolean[6];
+                    for (int j = 0; j < 6; j++) {
+                        int nx = x + dx[j];
+                        int ny = y + dy[j];
+                        for (int l = 1; l <= 5; l++) {
+                            if (b[nx][ny] == l) {
+                                check2[l] = true;
                             }
                         }
                     }
+                    int min2 = -1;
+                    int temp2 = 0;
+                    for (int j = 1; j < 6; j++) {
+                        if (min2 == -1 || min2 > r[j]) {
+                            if (check2[j] == false) {
+                                min2 = r[j];
+                                temp2 = j;
+                            }
+                        }
+                    }
+                    b[x][y] = temp2;
+                    r[temp2]++;
                 }
             }
         }
-//        for (int i = 0; i < 51; i++) {
-//            for (int j = 0; j < 51; j++) {
-//                System.out.print(b[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
-            for (int i = 0; i < 101; i++) {
-                for (int j = 0; j < 101; j++) {
+            for (int i = 0; i < 501; i++) {
+                for (int j = 0; j < 501; j++) {
                     if (a[i][j] == n) {
                         System.out.println(b[i][j]);
                     }
