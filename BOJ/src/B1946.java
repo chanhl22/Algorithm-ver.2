8fb39b41211ecac499938e09c62f0edb8f54047f
chanhl22 <chanhlee@student.42seoul.kr>
2021-11-03 10:14:35 +0900

    1946 / 42:47

--- BOJ/src/B1946.java
+++ BOJ/src/B1946.java
@@ -1,8 +1,9 @@
+//playtime = 42:47
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 class Newcomer {
     int x; //서류
