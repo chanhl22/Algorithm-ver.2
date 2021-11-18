da0f27b8767fbf7bec7a2d941b264838528d1c4d
chanhl22 <chanhlee@student.42seoul.kr>
2021-11-18 19:04:02 +0900

    3273 / 15:00 / Add Another Solution

--- BOJ/src/B3273.java
+++ BOJ/src/B3273.java
@@ -1,5 +1,37 @@
 //playtime = 15:00
 
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class B3273 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] a = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            a[i] = sc.nextInt();
+//        }
+//        Arrays.sort(a);
+//        int x = sc.nextInt();
+//        int cnt = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (a[i] <= x / 2) {
+//                for (int j = n - 1; j > i; j--) {
+//                    if (a[i] + a[j] == x) {
+//                        cnt++;
+//                    } else if (a[i] + a[j] < x) {
+//                        break;
+//                    }
+//                }
+//            } else {
+//                break;
+//            }
+//        }
+//        System.out.println(cnt);
+//    }
+//}
+
+//투포인터
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,20 +45,30 @@ public class B3273 {
         }
         Arrays.sort(a);
         int x = sc.nextInt();
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] <= x / 2) {
-                for (int j = n - 1; j > i; j--) {
-                    if (a[i] + a[j] == x) {
-                        cnt++;
-                    } else if (a[i] + a[j] < x) {
-                        break;
-                    }
-                }
+        int left = 0, right = n - 1;
+        int ans = 0;
+        while(left < right) {
+            int sum = a[left] + a[right];
+            if (sum < x) {
+                left++;
+            } else if (sum == x) {
+                ans++;
+                left++;
             } else {
-                break;
+                right--;
             }
+            /*
+            if(sum == x) {
+				ans++;
+			}
+			if(sum <= x) {
+				left++;
+			}
+			else {
+				right--;
+			}
+             */
         }
-        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
