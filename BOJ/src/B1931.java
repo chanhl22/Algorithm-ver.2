abd7e6d67fbafa1458fc9065997b8ae2e5958968
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-10 09:16:29 +0900

    1931 / 22:05

---
+++ BOJ/src/B1931.java
@@ -0,0 +1,43 @@
+//playtime : 22:05
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Pair36 {
+    int x;
+    int y;
+
+    public Pair36(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class B1931 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Pair36[] a = new Pair36[n];
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            a[i] = new Pair36(x,y);
+        }
+        Arrays.sort(a, (o1, o2) -> {
+            if(o1.y - o2.y == 0) {
+                return o1.x - o2.x;
+            } else {
+                return o1.y - o2.y;
+            }
+        });
+        int ans = 1;
+        int s = a[0].y;
+        for (int i = 1; i < n; i++) {
+            if (a[i].x >= s) {
+                s = a[i].y;
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
