00ae14299d77e0e2efa8fb464c237122784025e4
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-30 18:05:03 +0900

    1:00

---
+++ BOJ/src/B1202.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B1202 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] s = str.split(",");
+        System.out.println(s.length);
+    }
+}
