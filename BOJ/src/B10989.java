97a2beecc5bee7d5b4496e5cdb13f8278df48292
chanhl22 <chanhlee@student.42seoul.kr>
2022-02-21 23:00:37 +0900

    34:00 / 메모리 초과 수정

--- BOJ/src/B10989.java
+++ BOJ/src/B10989.java
@@ -15,7 +15,7 @@ public class B10989 {
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i < 10001; i++){
             while(arr[i] > 0){
-                sb.append(i + "\n");
+                sb.append(i).append('\n');
                 arr[i]--;
             }
         }
