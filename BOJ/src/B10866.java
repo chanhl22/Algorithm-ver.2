ac4e3627aa8fa15f7fb42913a815eca1c6e475f2
chanhl22 <chanhlee@student.42seoul.kr>
2022-02-23 13:30:43 +0900

    13:03

--- BOJ/src/B10866.java
+++ BOJ/src/B10866.java
@@ -1,3 +1,5 @@
+//playtime = 13:03
+
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
@@ -7,6 +9,7 @@ public class B10866 {
         int n = sc.nextInt();
         sc.nextLine();
         ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+        StringBuilder sb = new StringBuilder();
         for (int k=0; k<n; k++) {
             String line = sc.nextLine();
             String[] s = line.split(" ");
@@ -19,37 +22,38 @@ public class B10866 {
                 queue.offerLast(num);
             }  else if (cmd.equals("front")) {
                 if (queue.isEmpty()) {
-                    System.out.println("-1");
+                    sb.append(-1).append('\n');
                 } else {
-                    System.out.println(queue.peekFirst());
+                    sb.append(queue.peekFirst()).append('\n');
                 }
             } else if (cmd.equals("size")) {
-                System.out.println(queue.size());
+                sb.append(queue.size()).append('\n');
             } else if (cmd.equals("empty")) {
                 if (queue.isEmpty()) {
-                    System.out.println("1");
+                    sb.append(1).append('\n');
                 } else {
-                    System.out.println("0");
+                    sb.append(0).append('\n');
                 }
             } else if (cmd.equals("pop_front")) {
                 if (queue.isEmpty()) {
-                    System.out.println("-1");
+                    sb.append(-1).append('\n');
                 } else {
-                    System.out.println(queue.pollFirst());
+                    sb.append(queue.pollFirst()).append('\n');
                 }
             } else if (cmd.equals("pop_back")) {
                 if (queue.isEmpty()) {
-                    System.out.println("-1");
+                    sb.append(-1).append('\n');
                 } else {
-                    System.out.println(queue.pollLast());
+                    sb.append(queue.pollLast()).append('\n');
                 }
             } else if (cmd.equals("back")) {
                 if (queue.isEmpty()) {
-                    System.out.println("-1");
+                    sb.append(-1).append('\n');
                 } else {
-                    System.out.println(queue.peekLast());
+                    sb.append(queue.peekLast()).append('\n');
                 }
             }
         }
+        System.out.print(sb.toString());
     }
 }
