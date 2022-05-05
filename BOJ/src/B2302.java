904bb9ab94dd9093ae9a9a9744fe6564a6801904
chanhl22 <lch9502@naver.com>
2022-05-05 23:12:51 +0900

    극장 좌석 / 49:33

---
+++ BOJ/src/B2302.java
@@ -0,0 +1,38 @@
+//playtime = 49:33
+
+import java.util.Scanner;
+
+public class B2302 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[m + 1];
+        for (int i = 1; i <= m; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] dp = new int[41];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < 41; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        int count = 0;
+        int ans = 1;
+        int index = 1;
+        for (int i = 1; i <= n; i++) {
+            if (index <= m && arr[index] == i) {
+                index++;
+                ans *= dp[count];
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        if (count != 0) {
+            ans *= dp[count];
+        }
+        System.out.println(ans);
+    }
+}
