ad5916355c9ab0c0c68f0adb70c0b71c0594dbd7
chanhl22 <lch9502@naver.com>
2022-08-10 08:39:59 +0900

    좌표 압축 / 1:00:39

---
+++ BOJ/src/B18870.java
@@ -0,0 +1,45 @@
+//playtime = 1:00:39
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class CheckIndex {
+    int index;
+    int num;
+
+    public CheckIndex(int index, int num) {
+        this.index = index;
+        this.num = num;
+    }
+}
+
+public class B18870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<CheckIndex> pq = new PriorityQueue<>((o1, o2) -> o1.num - o2.num);
+        for (int i = 0; i < n; i++) {
+            pq.add(new CheckIndex(i, sc.nextInt()));
+        }
+        CheckIndex[] answer = new CheckIndex[n];
+        int idx = 0; //answer의 index
+        int temp = 0; //값이 같은 경우 ret을 증가시키지 않기 위한 임시 저장 변수
+        int ret = 0; //정답을 출력하기 위해 바뀌는 데이터
+        while (!pq.isEmpty()) {
+            CheckIndex remove = pq.remove();
+            if (temp == remove.num) {
+                ret--;
+            }
+            temp = remove.num;
+            remove.num = ret++;
+            answer[idx++] = remove;
+        }
+        Arrays.sort(answer, (o1, o2) -> o1.index - o2.index);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(answer[i].num).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
