1549f1e94bb3f05e6173e695042d06e3ef7fa349
chanhl22 <lch9502@naver.com>
2022-12-07 16:08:15 +0900

    문자열 나누기 / 01:54:02 / 문제를 잘못 이해함

--- Programmers/src/P140108.java
+++ Programmers/src/P140108.java
@@ -1,39 +1,32 @@
+//playtime = 01:54:02
+
 class Solution140108 {
     public int solution(String s) {
         int answer = 0;
-        int curCnt = 0;
-        int nextCnt = 0;
-        int curChar = s.charAt(0);
-        boolean cut = false;
+        Character fixedChar = null;
+        int cnt1 = 0;
+        int cnt2 = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (cut) {
-                curChar = s.charAt(i);
-                cut = false;
+            if (fixedChar == null) {
+                fixedChar = s.charAt(i);
             }
 
-            if (s.charAt(i) == curChar) {
-                curCnt++;
+            if (fixedChar == s.charAt(i)) {
+                cnt1++;
             } else {
-                if (nextCnt > 0) {
-                    nextCnt = 0;
-                }
-                nextCnt++;
-                curChar = s.charAt(i);
-                int temp = curCnt;
-                curCnt = nextCnt;
-                nextCnt = temp;
+                cnt2++;
             }
 
-            if (curCnt == nextCnt) {
+            if (cnt2 == cnt1) {
                 answer++;
-                curCnt = 0;
-                nextCnt = 0;
-                cut = true;
+                cnt2 = 0;
+                cnt1 = 0;
+                fixedChar = null;
             }
         }
 
-        if (curCnt != 0 || nextCnt != 0) {
+        if (fixedChar != null) {
             answer++;
         }
 
@@ -48,16 +41,6 @@ public class P140108 {
 //        ans = sol.solution("banana"); //3
 //        ans = sol.solution("abracadabra"); //6
 //        ans = sol.solution("aaabbaccccabba"); //3
-//        ans = sol.solution("baaa"); //2
-//        ans = sol.solution("baccaaccaa"); //3
-//        ans = sol.solution("ccaa"); //1
-//        ans = sol.solution("ccaaa"); //2
-//        ans = sol.solution("a"); //1
-//        ans = sol.solution("aaaaaaaaacb"); //1
-//        ans = sol.solution("aacb"); //1
-//        ans = sol.solution("abaabc"); //2
-//        ans = sol.solution("aaabbabb"); //2
-//        ans = sol.solution("aaabbc"); //1
         System.out.println(ans);
     }
 }
