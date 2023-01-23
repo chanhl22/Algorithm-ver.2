8983fb06fe206f89b9a76db50e14fe9df02fba73
chanhl22 <lch9502@naver.com>
2023-01-23 10:17:26 +0900

    기사단원의 무기 / 17:12

---
+++ Programmers/src/P136798.java
@@ -0,0 +1,44 @@
+//playtime = 17:12
+
+class Solution136798 {
+    public int solution(int number, int limit, int power) {
+        int[] divisor = new int[number];
+        init(divisor, number);
+
+        for (int i = 0; i < number; i++) {
+            if (divisor[i] > limit) {
+                divisor[i] = power;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < number; i++) {
+            answer += divisor[i];
+        }
+        return answer;
+    }
+
+    private void init(int[] divisor, int number) {
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) {
+                    count++;
+                } else if (i % j == 0) {
+                    count += 2;
+                }
+            }
+            divisor[i - 1] = count;
+        }
+    }
+}
+
+public class P136798 {
+    public static void main(String[] args) {
+        Solution136798 sol = new Solution136798();
+        int ans = 0;
+//        ans = sol.solution(5, 3, 2);
+        ans = sol.solution(10, 3, 2);
+        System.out.println(ans);
+    }
+}
