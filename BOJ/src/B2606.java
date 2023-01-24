148b3b216edbf00faa34f4eec49b76d6b35dc3d4
chanhl22 <lch9502@naver.com>
2023-01-24 09:43:27 +0900

    바이러스 / 19:26

--- BOJ/src/B2606.java
+++ BOJ/src/B2606.java
@@ -1,3 +1,6 @@
+//playtime = 19:26
+//풀이횟수 : 2
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -5,29 +8,31 @@ import java.util.Scanner;
 public class B2606 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int computer = sc.nextInt();
-        int connect = sc.nextInt();
-        int[][] a = new int[computer][computer];
-        for (int i = 0; i < connect; i++) {
-            int x = sc.nextInt()-1;
-            int y = sc.nextInt()-1;
-            a[x][y] = a[y][x] = 1;
+        int pcCount = sc.nextInt();
+        int pcPairCount = sc.nextInt();
+        int[][] connect = new int[pcCount][pcCount];
+        for (int i = 0; i < pcPairCount; i++) {
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+            connect[x][y] = connect[y][x] = 1;
         }
+
         Queue<Integer> q = new LinkedList<>();
-        boolean[] check = new boolean[computer];
         q.add(0);
+        boolean[] check = new boolean[pcCount];
         check[0] = true;
-        int ans = 0;
+
+        int answer = 0;
         while(!q.isEmpty()) {
             int x = q.remove();
-            for (int k = 0; k < computer; k++) {
-                if (a[x][k] == 1 && check[k] == false) {
-                    q.add(k);
-                    check[k] = true;
-                    ans++;
+            for (int i = 0; i < pcCount; i++) {
+                if (connect[x][i] == 1 && !check[i]) {
+                    q.add(i);
+                    check[i] = true;
+                    answer++;
                 }
             }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
