e1eb8cc28f0d5b26538e54cda3dcb23ecffb8178
chanhl22 <lch9502@naver.com>
2023-02-08 11:26:45 +0900

    둘만의 암호 / 01:56:26

--- Programmers/src/P155652.java
+++ Programmers/src/P155652.java
@@ -1,55 +1,53 @@
 /**
- * playtime = 13:14
- * 안풀림..
+ * playtime = 01:56:26
  */
 
 class Solution155652 {
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
-
-        char firstChar = 'a';
-        boolean check = false;
-        for (char i = 'a'; i < 'z'; i++) {
-            for (int j = 0; j < skip.length(); j++) {
-                if (i == skip.charAt(j)) {
-                    break;
-                }
-                check = true;
-            }
-            if (check) {
-                firstChar = i;
-                break;
-            }
-        }
+        int[] sArray = new int[s.length()];
+        int[] skipArray = new int[skip.length()];
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+            sArray[i] = s.charAt(i) - 'a';
+        }
+
+        for (int i = 0; i < skip.length(); i++) {
+            skipArray[i] = skip.charAt(i) - 'a';
+        }
+
+        for (int i = 0; (i < sArray.length); i++) {
             for (int j = 0; j < index; j++) {
-                c += 1;
-                if (c > 'z') {
-                    c = firstChar;
-                }
-                for (int k = 0; k < skip.length(); k++) {
-                    if (c == skip.charAt(k)) {
-                        c += 1;
-                        if (c > 'z') {
-                            c = firstChar;
-                        }
+                do {
+                    sArray[i]++;
+                    if (sArray[i] == 26) {
+                        sArray[i] = 0;
                     }
-                }
+                } while (findContains(sArray, skipArray));
             }
-            answer.append(c);
+            answer.append((char) (sArray[i] + 97));
         }
         return answer.toString();
+    }
+
+    private boolean findContains(int[] sArray, int[] skipArray) {
+        for (int s : sArray) {
+            for (int skip : skipArray) {
+                if (s == skip) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
 public class P155652 {
     public static void main(String[] args) {
         Solution155652 sol = new Solution155652();
-//        String ans = sol.solution("aukks", "wbqd", 5);
+        String ans = sol.solution("aukks", "wbqd", 5);
 //        String ans = sol.solution("ybcde", "az", 1); //bcdef
-        String ans = sol.solution("z", "abcdefghij", 20); //n
+//        String ans = sol.solution("z", "abcdefghij", 20); //n
         System.out.println(ans);
     }
 }
