8d5f01b0cf4af91b0568cd2af06f73d95da314a9
chanhl22 <lch9502@naver.com>
2023-02-23 16:33:28 +0900

    A Very Big Sum / 3m 49s

---
+++ HackerRank/src/AVeryBigSum.java
@@ -0,0 +1,41 @@
+/**
+ * playtime = 3m 49s
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static long aVeryBigSum(List<Long> ar) {
+        return ar.stream().mapToLong(i -> i).sum();
+    }
+
+}
+
+public class AVeryBigSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
