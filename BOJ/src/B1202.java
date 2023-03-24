272ea2fb89e0bc3717f142952a06a251c2743a82
chanhl22 <lch9502@naver.com>
2023-03-24 12:45:12 +0900

    1202

--- BOJ/src/B1202.java
+++ BOJ/src/B1202.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-class Jewel {
+class Jewel1202 {
     int weight;
     int price;
 
-    public Jewel(int weight, int price) {
+    public Jewel1202(int weight, int price) {
         this.weight = weight;
         this.price = price;
     }
@@ -19,12 +19,12 @@ public class B1202 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        Jewel[] jewels = new Jewel[n];
+        Jewel1202[] jewels = new Jewel1202[n];
         long[] bag = new long[k];
         for (int i = 0; i < n; i++) {
             int w = sc.nextInt();
             int p= sc.nextInt();
-            jewels[i] = new Jewel(w, p);
+            jewels[i] = new Jewel1202(w, p);
         }
         for (int i = 0; i < k; i++) {
             bag[i] = sc.nextLong();
