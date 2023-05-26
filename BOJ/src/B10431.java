59d7f9d600901395a6f10a2a7f74fea6be105084
chanhl22 <lch9502@naver.com>
2023-05-26 14:16:52 +0900

    삼각형과 세변 / 06:15

---
+++ BOJ/src/B10431.java
@@ -0,0 +1,69 @@
+/**
+ * playtime = 06:15
+ */
+
+import java.util.Scanner;
+
+class B10431 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+
+            if (finish(n1, n2, n3)) {
+                break;
+            }
+
+            if (isInvalid(n1, n2, n3)) {
+                System.out.println("Invalid");
+                continue;
+            }
+
+            if (isEquilateral(n1, n2, n3)) {
+                System.out.println("Equilateral");
+                continue;
+            }
+
+            if (isIsosceles(n1, n2, n3)) {
+                System.out.println("Isosceles");
+                continue;
+            }
+
+            if (isScalene(n1, n2, n3)) {
+                System.out.println("Scalene");
+            }
+        }
+    }
+
+    private static boolean isInvalid(int n1, int n2, int n3) {
+        if (n1 > n2 && n1 > n3) {
+            return n1 >= n2 + n3;
+        }
+        if (n2 > n1 && n2 > n3) {
+            return n2 >= n1 + n3;
+        }
+        if (n3 > n1 && n3 > n2) {
+            return n3 >= n1 + n2;
+        }
+        return false;
+    }
+
+    private static boolean isScalene(int n1, int n2, int n3) {
+        return n1 != n2 && n2 != n3;
+    }
+
+    private static boolean isIsosceles(int n1, int n2, int n3) {
+        return n1 == n2 || n2 == n3 || n1 == n3;
+    }
+
+    private static boolean isEquilateral(int n1, int n2, int n3) {
+        return n1 == n2 && n2 == n3;
+    }
+
+
+    private static boolean finish(int n1, int n2, int n3) {
+        return n1 == 0 && n2 == 0 && n3 == 0;
+    }
+}
