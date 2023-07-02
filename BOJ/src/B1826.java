adcb5b19a6594480f321f41d30b724d421acbfba
chanhl22 <lch9502@naver.com>
2023-07-02 13:26:16 +0900

    연료 채우기 / 03:38:06

--- BOJ/src/B1826.java
+++ BOJ/src/B1826.java
@@ -1,46 +1,63 @@
 /**
- * 01:01:28 못풀었음
+ * playtime = 03:38:06
  */
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+
+class GasStation implements Comparable<GasStation>{
+    int dist;
+    int gas;
+
+    public GasStation(int dist, int gas) {
+        this.dist = dist;
+        this.gas = gas;
+    }
+
+    public int getDist() {
+        return dist;
+    }
+
+    public int getGas() {
+        return gas;
+    }
+
+    @Override
+    public int compareTo(GasStation o) {
+        return this.dist - o.dist;
+    }
+}
 
 public class B1826 {
     public static void main(String[] args) {
-        /**
-         *
-         * dp[i] : i는 최소로 갈 수 있는 횟수
-         * gas[i] : 현재 위치에서 연료
-         */
-
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] gasStation = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            gasStation[i][0] = sc.nextInt();
-            gasStation[i][1] = sc.nextInt();
+        PriorityQueue<GasStation> gasStations = new PriorityQueue<>(Comparator.comparingInt(GasStation::getDist));
+        for (int i = 0; i < n; i++) {
+            gasStations.add(new GasStation(sc.nextInt(), sc.nextInt()));
         }
         int l = sc.nextInt();
         int p = sc.nextInt();
 
-        int[] dp = new int[n + 1];
-        int[] gas = new int[l + 1];
-        int startPoint = 0;
-        for (int i = 1; i <= n; i++) {
-            if (gasStation[i][0] <= p) {
-                dp[i] = 1;
-                startPoint = gasStation[i][0];
-                gas[i] = p - i + gasStation[i][1];
+        int answer = 0;
+
+        PriorityQueue<Integer> temp = new PriorityQueue<>(Collections.reverseOrder());
+        while (l > p) {
+            while (!gasStations.isEmpty() && gasStations.peek().getDist() <= p) {
+                temp.add(gasStations.remove().getGas());
             }
+
+            if (temp.isEmpty()) {
+                System.out.println(-1);
+                return;
+            }
+
+            p += temp.remove();
+            answer++;
         }
 
-        for (int i = startPoint + 1, j = 0; i <= l; i++) {
-            gas[i] = gas[i - 1] - 1;
-            if (gasStation[j++][0] == i) {
-                if (gas[i] >= 0) {
-                    dp[i] = dp[i - 1] + 1;
-                }
-            }
-        }
+        System.out.println(answer);
     }
 }
