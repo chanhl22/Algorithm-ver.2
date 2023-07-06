d866632e3dfd45c89a9514dcfe4a8187188ad405
chanhl22 <lch9502@naver.com>
2023-07-06 10:18:14 +0900

    인구 이동 / 39:48

--- BOJ/src/B16234.java
+++ BOJ/src/B16234.java
@@ -1,3 +1,103 @@
+/**
+ * playtime = 39:48
+ * 풀이횟수 = 2
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class B16234 {
+
+    private static int n;
+    private static int l;
+    private static int r;
+    private static int[][] people;
+    private final static int[] dx = {0, 0, -1, 1};
+    private final static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    private static void input() {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        l = sc.nextInt();
+        r = sc.nextInt();
+        people = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                people[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    private static void solution() {
+        int answer = 0;
+        while (true) {
+            if (bfs()) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean bfs() {
+        boolean isOk = false;
+        boolean[][] isVisit = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isVisit[i][j]) {
+                    continue;
+                }
+                isVisit[i][j] = true;
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                q.add(j);
+                Queue<Integer> store = new LinkedList<>();
+                store.add(i);
+                store.add(j);
+                int sum = people[i][j];
+                while (!q.isEmpty()) {
+                    int x = q.remove();
+                    int y = q.remove();
+                    for (int k = 0; k < 4; k++) {
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                            if (!isVisit[nx][ny]) {
+                                if (Math.abs(people[x][y] - people[nx][ny]) >= l && Math.abs(people[x][y] - people[nx][ny]) <= r) {
+                                    q.add(nx);
+                                    q.add(ny);
+                                    isVisit[nx][ny] = true;
+                                    store.add(nx);
+                                    store.add(ny);
+                                    sum += people[nx][ny];
+                                    isOk = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                int value = sum / (store.size() / 2);
+                while (!store.isEmpty()) {
+                    int x = store.remove();
+                    int y = store.remove();
+                    people[x][y] = value;
+                }
+            }
+        }
+        return isOk;
+    }
+}
+
+/* Standard solution
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -6,7 +106,7 @@ public class B16234 {
 	static int n;
 	static int[] dx = { 0, 0, 1, -1 };
 	static int[] dy = { 1, -1, 0, 0 };
-	
+
 	static boolean bfs(int[][] a, int l, int r) {
 		boolean[][] c = new boolean[n][n];
 		boolean ok = false;
@@ -56,7 +156,7 @@ public class B16234 {
 		}
 		return ok;
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
@@ -79,5 +179,5 @@ public class B16234 {
 		}
 		System.out.println(ans);
 	}
-
 }
+*/
