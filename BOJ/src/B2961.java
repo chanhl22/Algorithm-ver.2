8df74079e80db21a64d683a1b541143e577107b1
chanhl22 <lch9502@naver.com>
2023-07-14 22:03:16 +0900

    2961

--- BOJ/src/B2961.java
+++ BOJ/src/B2961.java
@@ -2,29 +2,29 @@
 
 import java.util.Scanner;
 
-class Cook {
+class Cook2961 {
     int t1;
     int t2;
 
-    public Cook(int t1, int t2) {
+    public Cook2961(int t1, int t2) {
         this.t1 = t1;
         this.t2 = t2;
     }
 }
 
 public class B2961 {
-    static Cook[] a;
+    static Cook2961[] a;
     static int n;
     static int ans = -1;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        a = new Cook[n];
+        a = new Cook2961[n];
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            a[i] = new Cook(x, y);
+            a[i] = new Cook2961(x, y);
         }
         go(0, 1, 0);
         System.out.println(ans);
