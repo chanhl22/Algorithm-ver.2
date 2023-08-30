5500896cce6bd6acaf8bbed9fc7a1b00d7a2d8ea
chanhl22 <lch9502@naver.com>
2023-08-30 13:57:46 +0900

    랭킹전 대기열 / 48:46

--- BOJ/src/B20006.java
+++ BOJ/src/B20006.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 
-class Room {
+class Room20006 {
     private int count;
-    private final ArrayList<Member> members = new ArrayList<>();
+    private final ArrayList<Member20006> members = new ArrayList<>();
 
-    public Room(Member member) {
+    public Room20006(Member20006 member) {
         members.add(member);
         count++;
     }
@@ -19,21 +19,21 @@ class Room {
         return count;
     }
 
-    public ArrayList<Member> getMembers() {
+    public ArrayList<Member20006> getMembers() {
         return members;
     }
 
-    public void setMember(Member member) {
+    public void setMember(Member20006 member) {
         members.add(member);
         count++;
     }
 }
 
-class Member {
+class Member20006 {
     private int level;
     private String name;
 
-    public Member(int level, String name) {
+    public Member20006(int level, String name) {
         this.level = level;
         this.name = name;
     }
@@ -50,8 +50,8 @@ class Member {
 public class B20006 {
     private static int p;
     private static int m;
-    private static final ArrayList<Room> rooms = new ArrayList<>();
-    private static final ArrayList<Member> members = new ArrayList<>();
+    private static final ArrayList<Room20006> rooms = new ArrayList<>();
+    private static final ArrayList<Member20006> members = new ArrayList<>();
 
     public static void main(String[] args) {
         input();
@@ -64,7 +64,7 @@ public class B20006 {
         p = sc.nextInt();
         m = sc.nextInt();
         for (int i = 0; i < p; i++) {
-            members.add(new Member(sc.nextInt(), sc.next()));
+            members.add(new Member20006(sc.nextInt(), sc.next()));
         }
     }
 
@@ -78,17 +78,17 @@ public class B20006 {
     }
 
     private static void print() {
-        for (Room room : rooms) {
+        for (Room20006 room : rooms) {
             if (room.getCount() == m) {
                 System.out.println("Started!");
-                room.getMembers().sort(Comparator.comparing(Member::getName));
-                for (Member member : room.getMembers()) {
+                room.getMembers().sort(Comparator.comparing(Member20006::getName));
+                for (Member20006 member : room.getMembers()) {
                     System.out.println(member.getLevel() + " " + member.getName());
                 }
             } else {
                 System.out.println("Waiting!");
-                room.getMembers().sort(Comparator.comparing(Member::getName));
-                for (Member member : room.getMembers()) {
+                room.getMembers().sort(Comparator.comparing(Member20006::getName));
+                for (Member20006 member : room.getMembers()) {
                     System.out.println(member.getLevel() + " " + member.getName());
                 }
             }
@@ -96,11 +96,11 @@ public class B20006 {
     }
 
     private static void makeRoom(int index) {
-        rooms.add(new Room(members.get(index)));
+        rooms.add(new Room20006(members.get(index)));
     }
 
     private static boolean findRoomAndEnterRoom(int index) {
-        for (Room room : rooms) {
+        for (Room20006 room : rooms) {
             if (room.getCount() < m && checkLevel(members.get(index).getLevel(), room.getMembers().get(0).getLevel())) {
                 room.setMember(members.get(index));
                 return true;
