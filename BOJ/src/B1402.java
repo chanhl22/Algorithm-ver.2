43c5579861f96f3dab146afa30662a83a1924ddb
chanhl22 <lch9502@naver.com>
2023-09-20 16:22:24 +0900

    아무래도이문제는A번난이도인것같다 / 48:44

---
+++ BOJ/src/B1402.java
@@ -0,0 +1,18 @@
+/**
+ * playtime = 48:44
+ */
+
+import java.util.Scanner;
+
+public class B1402 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+
+            System.out.println("yes");
+        }
+    }
+}
