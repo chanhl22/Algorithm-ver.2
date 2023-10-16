7cafc2d81eb7c25a79a43b12811fb0a5a429ee4a
chanhl22 <lch9502@naver.com>
2023-10-16 22:49:33 +0900

    18405

--- BOJ/src/B18405.java
+++ BOJ/src/B18405.java
@@ -2,12 +2,12 @@
 
 import java.util.*;
 
-class Pair39 {
+class Pair18405 {
     int x;
     int y;
     int virus;
 
-    public Pair39(int x, int y, int virus) {
+    public Pair18405(int x, int y, int virus) {
         this.x = x;
         this.y = y;
         this.virus = virus;
@@ -20,12 +20,12 @@ public class B18405 {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[][] map = new int[n][n];
-        ArrayList<Pair39> virus = new ArrayList<>();
+        ArrayList<Pair18405> virus = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 map[i][j] = sc.nextInt();
                 if (map[i][j] != 0) {
-                    virus.add(new Pair39(i, j, map[i][j]));
+                    virus.add(new Pair18405(i, j, map[i][j]));
                 }
             }
         }
@@ -39,7 +39,7 @@ public class B18405 {
         int y = sc.nextInt() - 1;
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
-        Queue<Pair39> q = new LinkedList<>();
+        Queue<Pair18405> q = new LinkedList<>();
         for (int i = 0; i < virus.size(); i++) {
             q.add(virus.get(i));
         }
@@ -49,7 +49,7 @@ public class B18405 {
             visited[tx][ty] = 0;
         }
         while (!q.isEmpty()) {
-            Pair39 p = q.remove();
+            Pair18405 p = q.remove();
             int sx = p.x;
             int sy = p.y;
             int sv = p.virus;
@@ -58,7 +58,7 @@ public class B18405 {
                 int ny = sy + dy[t];
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
                     if (visited[nx][ny] == -1 && map[nx][ny] == 0) {
-                        q.add(new Pair39(nx, ny, sv));
+                        q.add(new Pair18405(nx, ny, sv));
                         visited[nx][ny] = visited[sx][sy] + 1;
                         map[nx][ny] = sv;
                     }
