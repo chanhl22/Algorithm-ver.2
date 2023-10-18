79d671cce1573629fcb0895d1508e1242f828617
chanhl22 <lch9502@naver.com>
2023-10-18 15:12:17 +0900

    국영수 / 10:59

---
+++ BOJ/src/B10825.java
@@ -0,0 +1,61 @@
+/**
+ * playtime = 10:59
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Score {
+    String name;
+    int korean;
+    int english;
+    int math;
+
+    public Score(String name, int korean, int english, int math) {
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+}
+
+public class B10825 {
+
+    private static List<Score> scores = new ArrayList<>();
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    private static void input() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            scores.add(new Score(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        }
+    }
+
+    private static void solution() {
+        scores.sort((o1, o2) -> {
+            if (o1.korean == o2.korean) {
+                if (o1.english == o2.english) {
+                    if (o1.math == o2.math) {
+                        return o1.name.compareTo(o2.name);
+                    } else {
+                        return o2.math - o1.math;
+                    }
+                } else {
+                    return o1.english - o2.english;
+                }
+            } else {
+                return o2.korean - o1.korean;
+            }
+        });
+
+        for (Score score : scores) {
+            System.out.println(score.name);
+        }
+    }
+}
