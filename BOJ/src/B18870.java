addb253836b89be08de80140bf36f789101b02e7
chanhl22 <lch9502@naver.com>
2023-10-21 10:56:23 +0900

    18870

--- BOJ/src/B18870.java
+++ BOJ/src/B18870.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-class CheckIndex {
+class CheckIndex18870 {
     int index;
     int num;
 
-    public CheckIndex(int index, int num) {
+    public CheckIndex18870(int index, int num) {
         this.index = index;
         this.num = num;
     }
@@ -18,16 +18,16 @@ public class B18870 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        PriorityQueue<CheckIndex> pq = new PriorityQueue<>((o1, o2) -> o1.num - o2.num);
+        PriorityQueue<CheckIndex18870> pq = new PriorityQueue<>((o1, o2) -> o1.num - o2.num);
         for (int i = 0; i < n; i++) {
-            pq.add(new CheckIndex(i, sc.nextInt()));
+            pq.add(new CheckIndex18870(i, sc.nextInt()));
         }
-        CheckIndex[] answer = new CheckIndex[n];
+        CheckIndex18870[] answer = new CheckIndex18870[n];
         int idx = 0; //answer의 index
         int temp = 0; //값이 같은 경우 ret을 증가시키지 않기 위한 임시 저장 변수
         int ret = 0; //정답을 출력하기 위해 바뀌는 데이터
         while (!pq.isEmpty()) {
-            CheckIndex remove = pq.remove();
+            CheckIndex18870 remove = pq.remove();
             if (temp == remove.num) {
                 ret--;
             }
