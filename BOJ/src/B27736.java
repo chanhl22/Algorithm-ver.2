cab605dd8c605612ae4b6eea669368e67772144e
chanhl22 <lch9502@naver.com>
2024-01-12 08:37:59 +0900

    찬반투표 / 09m 33s

--- BOJ/src/B27736.java
+++ BOJ/src/B27736.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class B27736 {
+/**
+ * playtime = 09m 33s
+ */
+
+import java.util.Scanner;
+
+public class B27736 {
+
+    private static int n;
+    private static int[] result;
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    private static void solution() {
+        int agreement = 0;
+        int opposition = 0;
+        int abstention = 0;
+        for (int i = 0; i < n; i++) {
+            if (result[i] == 1) {
+                agreement++;
+            } else if (result[i] == -1) {
+                opposition++;
+            } else {
+                abstention++;
+            }
+        }
+
+        if (n % 2 == 0) {
+            if (n / 2 <= abstention) {
+                System.out.println("INVALID");
+                System.exit(0);
+            }
+        } else {
+            if (n / 2 + 1 <= abstention) {
+                System.out.println("INVALID");
+                System.exit(0);
+            }
+        }
+
+        if (agreement > opposition) {
+            System.out.println("APPROVED");
+        } else {
+            System.out.println("REJECTED");
+        }
+    }
+
+    private static void input() {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = sc.nextInt();
+        }
+    }
 }
