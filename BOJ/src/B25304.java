c4f8e1dac80068e019d6adbd5c58f0fc93fbb54e
chanhl22 <lch9502@naver.com>
2024-01-17 21:59:15 +0900

    영수증 / 02m 04s

---
+++ BOJ/src/B25304.java
@@ -0,0 +1,25 @@
+/**
+ * playtime = 02m 04s
+ */
+
+import java.util.Scanner;
+
+public class B25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a * b;
+        }
+
+        if (x == sum) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
