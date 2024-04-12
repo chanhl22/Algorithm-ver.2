426f3a1fcbd9d16d19dc983fe792f65c81657354
chanhl22 <lch9502@naver.com>
2024-04-12 17:24:07 +0900

    암호 만들기 / 26:23

--- BOJ/src/B1759.java
+++ BOJ/src/B1759.java
@@ -1,5 +1,5 @@
 /**
- * playtime =
+ * playtime = 26:23
  * 풀이횟수 = 1
  */
 
@@ -7,52 +7,112 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class B1759 {
-    static boolean check(String password) {
-        int mo = 0;
-        int ja = 0;
-        char[] x = password.toCharArray();
-        for (int i = 0; i < password.length(); i++) {
-		/*
-		for (char x : password.toCharArray())
-		*/
-            if (x[i] == 'a' || x[i] == 'e' || x[i] == 'i' || x[i] == 'o' || x[i] == 'u')
-                mo += 1;
-            else
-                ja += 1;
-        }
 
-        if (mo >= 1 && ja >= 2) {
-            return true;
-        } else {
-            return false;
-        }
-        //return mo >= 1 && ja >= 2;
+    private static int l;
+    private static int c;
+    private static String[] alpha;
+
+    public static void main(String[] args) {
+        input();
+        solution();
     }
 
-    static void go(int n, String[] alpha, String password, int i) {
-        if (password.length() == n) {
-            if (check(password)) {
+    private static void solution() {
+        recursive(0, "");
+    }
+
+    private static void recursive(int index, String password) {
+        if (password.length() == l) {
+            if (checkPassword(password)) {
                 System.out.println(password);
             }
             return;
         }
-        if (i >= alpha.length) {
+
+        if (index >= c) {
             return;
         }
-        go(n, alpha, password + alpha[i], i + 1);
-        go(n, alpha, password, i + 1);
+
+        recursive(index + 1, password + alpha[index]);
+        recursive(index + 1, password);
     }
 
-    public static void main(String[] args) {
+    private static boolean checkPassword(String password) {
+        int mo = 0;
+        int ja = 0;
+        char[] chars = password.toCharArray();
+        for (char s : chars) {
+            if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u') {
+                mo++;
+            } else {
+                ja++;
+            }
+        }
+        return mo >= 1 && ja >= 2;
+    }
+
+    private static void input() {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String[] alpha = new String[m];
-        for (int i = 0; i < m; i++) {
+        l = sc.nextInt();
+        c = sc.nextInt();
+        alpha = new String[c];
+        for (int i = 0; i < c; i++) {
             alpha[i] = sc.next();
         }
-
         Arrays.sort(alpha);
-        go(n, alpha, "", 0);
     }
-}		
+}
+
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class B1759 {
+//    static boolean check(String password) {
+//        int mo = 0;
+//        int ja = 0;
+//        char[] x = password.toCharArray();
+//        for (int i = 0; i < password.length(); i++) {
+//		/*
+//		for (char x : password.toCharArray())
+//		*/
+//            if (x[i] == 'a' || x[i] == 'e' || x[i] == 'i' || x[i] == 'o' || x[i] == 'u')
+//                mo += 1;
+//            else
+//                ja += 1;
+//        }
+//
+//        if (mo >= 1 && ja >= 2) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//        //return mo >= 1 && ja >= 2;
+//    }
+//
+//    static void go(int n, String[] alpha, String password, int i) {
+//        if (password.length() == n) {
+//            if (check(password)) {
+//                System.out.println(password);
+//            }
+//            return;
+//        }
+//        if (i >= alpha.length) {
+//            return;
+//        }
+//        go(n, alpha, password + alpha[i], i + 1);
+//        go(n, alpha, password, i + 1);
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        String[] alpha = new String[m];
+//        for (int i = 0; i < m; i++) {
+//            alpha[i] = sc.next();
+//        }
+//
+//        Arrays.sort(alpha);
+//        go(n, alpha, "", 0);
+//    }
+//}
