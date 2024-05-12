f5a604fc8c2e63c5608f2fbbb652c2227b6f7c7e
chanhl22 <lch9502@naver.com>
2024-05-12 18:07:43 +0900

    superblock

---
+++ Programmers/src/question/superblock/Superblock3.java
@@ -0,0 +1,18 @@
+package question.superblock;
+
+public class Superblock3 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int ans = 0;
+        ans = sol.solution(new int[]{1, 1, 1, 1, 1}, 3);
+        System.out.println(ans);
+    }
+
+    static class Solution {
+        public int solution(int[] numbers, int target) {
+            int answer = 0;
+
+            return answer;
+        }
+    }
+}
