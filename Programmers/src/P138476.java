ca74f51a294719cf18951ccff406548f1b3c8ae6
chanhl22 <lch9502@naver.com>
2024-05-13 16:42:21 +0900

    귤 고르기 / 46m 44s

--- Programmers/src/P138476.java
+++ Programmers/src/P138476.java
@@ -1,4 +1,69 @@
-//playtime = 28:02
+/**
+ * playtime = 46m 44s
+ * 풀이횟수 = 2
+ */
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P138476 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int ans = 0;
+//        ans = sol.solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
+        ans = sol.solution(4, new int[]{1, 3, 2, 5, 4, 5, 2, 3});
+//        ans = sol.solution(2, new int[]{1, 1, 1, 1, 2, 2, 2, 3});
+        System.out.println(ans);
+    }
+
+    static class Fruit {
+        int size;
+        long count;
+
+        public Fruit(int size, long count) {
+            this.size = size;
+            this.count = count;
+        }
+
+        public long getCount() {
+            return count;
+        }
+    }
+
+    static class Solution {
+        public int solution(int k, int[] tangerine) {
+            int answer = 0;
+            List<Fruit> fruits = initFruit(tangerine);
+
+            for (Fruit fruit : fruits) {
+                k -= fruit.count;
+                answer++;
+                if (k <= 0) {
+                    break;
+                }
+            }
+            return answer;
+        }
+
+        private List<Fruit> initFruit(int[] tangerine) {
+            Map<Integer, Long> map = Arrays.stream(tangerine)
+                    .boxed()
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+            return map.entrySet().stream()
+                    .map(entry -> new Fruit(entry.getKey(), entry.getValue()))
+                    .sorted(Comparator.comparing(Fruit::getCount, Comparator.reverseOrder()))
+                    .collect(Collectors.toList());
+        }
+    }
+}
+
+/**
+ * playtime = 28:02
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -54,3 +119,4 @@ public class P138476 {
         System.out.println(ans);
     }
 }
+ */
