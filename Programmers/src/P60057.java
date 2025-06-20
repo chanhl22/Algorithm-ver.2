88e5c42de161d8df85b92b1e5c93cf6dd8c0a00e
chanhl22 <lch9502@naver.com>
2025-06-20 23:42:46 +0900

    문자열 압축 / 1s 13m 49s / 리팩토링

--- Programmers/src/P60057.java
+++ Programmers/src/P60057.java
@@ -35,38 +35,32 @@ public class P60057 {
         private int split(int count, String s) {
             StringBuilder sb = new StringBuilder();
 
-            List<String> storage = new ArrayList<>();
+            List<String> units = new ArrayList<>();
             for (int i = 0; i < s.length(); i += count) {
                 int end = Math.min(i + count, s.length());
-                storage.add(s.substring(i, end));
+                units.add(s.substring(i, end));
             }
 
-            int curInt = 1;
-            for (int i = 0; i < storage.size() - 1; i++) {
-                String curString = storage.get(i);
-                String nextString = storage.get(i + 1);
-                if (curString.equals(nextString)) {
-                    curInt++;
-
-                    if (i == storage.size() - 2) {
-                        sb.append(curInt);
-                        sb.append(curString);
-                    }
-
+            String prev = units.get(0);
+            int repeatCount = 1;
+            for (int i = 1; i < units.size(); i++) {
+                String curr = units.get(i);
+                if (prev.equals(curr)) {
+                    repeatCount++;
                 } else {
-                    if (curInt != 1) {
-                        sb.append(curInt);
+                    if (repeatCount > 1) {
+                        sb.append(repeatCount);
                     }
-                    sb.append(curString);
-
-                    if (i == storage.size() - 2) {
-                        sb.append(nextString);
-                    }
-
-                    curInt = 1;
+                    sb.append(prev);
+                    prev = curr;
+                    repeatCount = 1;
                 }
-
             }
+
+            if (repeatCount > 1) {
+                sb.append(repeatCount);
+            }
+            sb.append(prev);
 
             return sb.length();
         }
