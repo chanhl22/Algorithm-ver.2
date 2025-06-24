1453856bb639b097abb61d071f2527d346504d68
chanhl22 <lch9502@naver.com>
2025-06-24 22:06:03 +0900

    feat: 코틀린 샘플 코드

---
+++ Programmers/src/SampleKotlin.kt
@@ -0,0 +1,18 @@
+class SampleKotlin {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val sol = Solution()
+            val ans = sol.solution(intArrayOf(1, 1, 1, 1, 1), 3)
+            println(ans)
+        }
+    }
+
+    class Solution {
+        fun solution(numbers: IntArray, target: Int): Int {
+            var answer = 0
+            return answer
+        }
+    }
+
+}
