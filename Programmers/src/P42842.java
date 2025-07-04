cb68421cd1527929ebad622567c62ca7ebb7c644
chanhl22 <lch9502@naver.com>
2025-07-04 23:21:15 +0900

    카펫 / 16m 18s

--- Programmers/src/P42842.java
+++ Programmers/src/P42842.java
@@ -1,29 +1,34 @@
+/**
+ * 카펫
+ * playtime = 16m 18s
+ */
+
 import java.util.Arrays;
-
-class Solution18 {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        for (int i = 1; i <= yellow; i++) {
-            if (yellow % i == 0) {
-                int row = i;
-                int col = yellow / i;
-                if (row >= col && brown == row + row + 2 + 2 + col + col) {
-                    answer[0] = row + 2;
-                    answer[1] = col + 2;
-                }
-            }
-        }
-        return answer;
-    }
-}
-
 
 public class P42842 {
     public static void main(String[] args) {
-        Solution18 sol = new Solution18();
+        Solution sol = new Solution();
         //int[] ans = sol.solution(10, 2);
         //int[] ans = sol.solution(8, 1);
         int[] ans = sol.solution(24, 24);
         System.out.println(Arrays.toString(ans));
     }
+
+    static class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = new int[2];
+            for (int i = 1; i <= yellow; i++) {
+                if (yellow % i == 0) {
+                    int row = i;
+                    int col = yellow / i;
+                    if (row >= col && brown == row * 2 + col * 2 + 4) {
+                        answer[0] = row + 2;
+                        answer[1] = col + 2;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+
 }
