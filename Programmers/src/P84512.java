4d7ff232680a0310d2b6094e380e0a01ab96f3d9
chanhl22 <lch9502@naver.com>
2025-07-16 22:12:08 +0900

    모음사전 / 16m 19s

---
+++ Programmers/src/P84512.java
@@ -0,0 +1,48 @@
+/**
+ * 모음사
+ * playtime = 16m 19s
+ */
+
+import java.util.List;
+
+public class P84512 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int ans = 0;
+//        ans = sol.solution("AAAAE");
+        ans = sol.solution("AAAE");
+        System.out.println(ans);
+    }
+
+    static class Solution {
+
+        private static int COUNT = -1;
+        private static String FINISH;
+        private static List<String> ALPHABET = List.of("A", "E", "I", "O", "U");
+        private static int ANSWER;
+
+        public int solution(String word) {
+            FINISH = word;
+
+            go(0, "");
+
+            return ANSWER;
+        }
+
+        private void go(int index, String str) {
+            COUNT++;
+
+            if (str.equals(FINISH)) {
+                ANSWER = COUNT;
+            }
+
+            if (index == 5) {
+                return;
+            }
+
+            for (int i = 0; i < ALPHABET.size(); i++) {
+                go(index + 1, str + ALPHABET.get(i));
+            }
+        }
+    }
+}
