5b4bde7a586662fe77f433c14d2ba76a95ff7d98
chanhl22 <lch9502@naver.com>
2025-07-22 17:01:00 +0900

    가장 가까운 같은 글자 / 10m 24s

--- Programmers/src/P142086.java
+++ Programmers/src/P142086.java
@@ -1,27 +1,33 @@
+/**
+ * playtime = 10m 24s
+ */
+
 import java.util.Arrays;
-
-class Solution142086 {
-    public int[] solution(String s) {
-        int[] answer = new int[s.length()];
-
-        int[] alpha = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            if (alpha[s.charAt(i) - 'a'] == 0) {
-                answer[i] = -1;
-            } else {
-                answer[i] = i + 1- alpha[s.charAt(i) - 'a'];
-            }
-            alpha[s.charAt(i) - 'a'] = i + 1;
-        }
-
-        return answer;
-    }
-}
 
 public class P142086 {
     public static void main(String[] args) {
-        Solution142086 sol = new Solution142086();
-        int[] ans = sol.solution("banana"); //[-1, -1, -1, 2, 2, 2]
+        Solution sol = new Solution();
+        int[] ans = sol.solution("banana");
         System.out.println(Arrays.toString(ans));
+    }
+
+    static class Solution {
+        public int[] solution(String s) {
+            int[] answer = new int[s.length()];
+            int[] alphaIndex = new int[26];
+            Arrays.fill(alphaIndex, -1);
+
+            for (int i = 0; i < s.length(); i++) {
+                int index = alphaIndex[s.charAt(i) - 'a'];
+                if (index == -1) {
+                    answer[i] = -1;
+                } else {
+                    answer[i] = i - index;
+                }
+                alphaIndex[s.charAt(i) - 'a'] = i;
+            }
+
+            return answer;
+        }
     }
 }
