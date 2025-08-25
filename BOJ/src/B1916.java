12b4ef5567e607941969990862e39ebe86d3aab2
chanhl22 <lch9502@naver.com>
2025-08-25 22:03:38 +0900

    최소비용 구하기 / 51m 26s

---
+++ BOJ/src/B1916.java
@@ -0,0 +1,80 @@
+/**
+ * playtime = 51m 26s
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class B1916 {
+    static class Node implements Comparable<Node> {
+        int to, cost;
+
+        Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static int N, M;
+    static List<Node>[] graph;
+    static int[] dist;
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new ArrayList[N + 1];
+        dist = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+            dist[i] = INF;
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b, c));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start);
+
+        System.out.println(dist[end]);
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (dist[now.to] < now.cost) continue;
+
+            for (Node next : graph[now.to]) {
+                int newDist = dist[now.to] + next.cost;
+                if (newDist < dist[next.to]) {
+                    dist[next.to] = newDist;
+                    pq.offer(new Node(next.to, newDist));
+                }
+            }
+        }
+    }
+}
