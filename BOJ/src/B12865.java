604f498a6577a8fc375bb18018a382f40cddc827
chanhl22 <lch9502@naver.com>
2025-09-15 19:25:29 +0900

    평범한 배낭 / 20m 24s

--- BOJ/src/B12865.java
+++ BOJ/src/B12865.java
@@ -1,4 +1,6 @@
-//playtime = 29:28
+/**
+ * playtime = 20m 24s
+ */
 
 import java.util.Scanner;
 
@@ -13,9 +15,7 @@ public class B12865 {
             w[i] = sc.nextInt();
             v[i] = sc.nextInt();
         }
-        //D[i][j] : i번째 물건까지 고려했고, 배낭에 넣은 물건 무게의 합이 j일 때, 가치의 최대값
-        //1. i번째 물건을 넣지 않을 경우 : D[i-1][j]
-        //2. i번째 물건을 넣을 경우 : D[i-1][j-w[i]] + v[i];
+
         int[][] d = new int[n + 1][k + 1];
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <= k; j++) {
@@ -28,6 +28,37 @@ public class B12865 {
         System.out.println(d[n][k]);
     }
 }
+
+////playtime = 29:28
+//
+//import java.util.Scanner;
+//
+//public class B12865 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int k = sc.nextInt();
+//        int[] w = new int[n + 1];
+//        int[] v = new int[n + 1];
+//        for (int i = 1; i <= n; i++) {
+//            w[i] = sc.nextInt();
+//            v[i] = sc.nextInt();
+//        }
+//        //D[i][j] : i번째 물건까지 고려했고, 배낭에 넣은 물건 무게의 합이 j일 때, 가치의 최대값
+//        //1. i번째 물건을 넣지 않을 경우 : D[i-1][j]
+//        //2. i번째 물건을 넣을 경우 : D[i-1][j-w[i]] + v[i];
+//        int[][] d = new int[n + 1][k + 1];
+//        for (int i = 1; i <=n ; i++) {
+//            for (int j = 1; j <= k; j++) {
+//                d[i][j] = d[i-1][j];
+//                if (j - w[i] >= 0) {
+//                    d[i][j] = Math.max(d[i][j], d[i-1][j-w[i]] + v[i]);
+//                }
+//            }
+//        }
+//        System.out.println(d[n][k]);
+//    }
+//}
 
 //import java.util.*;
 //public class Main {
