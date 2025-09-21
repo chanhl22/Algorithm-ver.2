6a88c1e5d0931814f28e364d3dd31412239e23ad
chanhl22 <lch9502@naver.com>
2025-09-21 14:28:14 +0900

    카드2 / 5m 59s

--- BOJ/src/B2164.java
+++ BOJ/src/B2164.java
@@ -1,20 +1,63 @@
+/**
+ * playtime = 5m 59s
+ */
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class B2164 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = input();
+
         Queue<Integer> q = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             q.add(i);
         }
-        while(q.size() != 1) {
-            q.remove();
-            int back = q.remove();
-            q.add(back);
+
+        while(true) {
+            if (q.size() == 1) {
+                System.out.println(q.peek());
+                return;
+            }
+            cal1(q);
+            cal2(q);
         }
-        System.out.println(q.peek());
+    }
+
+    private static void cal1(Queue<Integer> q) {
+        q.remove();
+    }
+
+    private static void cal2(Queue<Integer> q) {
+        Integer remove = q.remove();
+        q.add(remove);
+    }
+
+    private static int input() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        return n;
     }
 }
+
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.Scanner;
+//
+//public class B2164 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        Queue<Integer> q = new LinkedList<>();
+//        for (int i = 1; i <= n; i++) {
+//            q.add(i);
+//        }
+//        while(q.size() != 1) {
+//            q.remove();
+//            int back = q.remove();
+//            q.add(back);
+//        }
+//        System.out.println(q.peek());
+//    }
+//}
